d3b988af4205644816a58191d390f875e40e5829
unknown <promesse@octan.group>
2020-09-16 15:57:13 +0200

    Start the project and create a database connection

---
+++ src/index.js
@@ -0,0 +1,15 @@
+const express = require ('express')
+const path = require ('path')
+require('../db/mongoose')
+require('dotenv')
+
+
+const app = express()
+const port = process.env.PORT || 3000
+const publicDirectoryPath = path.join(__dirname, '../public')
+
+app.use(express.static(publicDirectoryPath))
+
+app.listen(port, ()=>{
+    console.log(`The server is up and running on ${port}!`)
+})
