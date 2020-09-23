79c3413c363c4cbb2ba015530c7faa63e29ba539
unknown <promesse@octan.group>
2020-09-23 15:42:05 +0200

    Hash the password of the employer and generate token

--- models/Employer.js
+++ models/Employer.js
@@ -1,5 +1,7 @@
 const mongoose = require('mongoose')
 const validator = require('validator')
+const bcrypt = require('bcryptjs')
+const jw = require('jsonwebtoken')
 
 const employerSchema = mongoose.Schema({
     username:{
@@ -37,9 +39,33 @@ const employerSchema = mongoose.Schema({
         required:true,
         minlength:8
     },
+    tokens:[{
+        token:{
+            type:String,
+            required:true
+        }
+     }],
+},{
     timestamps:true
-
 })
+
+// hashing the password
+employerSchema.pre('save',async function(next){
+    const employer = this
+    if(employerSchema.isModified('password')){
+        employer.password = await bcrypt.hash(employer.password, 8)
+    }
+    next()
+})
+
+// generate authtoken
+employerSchema.methods.generateAuthToken = async function(){
+    const employer = this
+    const token = jwt.sign({_id:employer._id.toString()}, process.env.JWT_SECRET)
+   employer.tokens = employer.tokens.concat({token})
+    await employer.save()
+    return token
+}
 
 const Employer = mongoose.model('Employer', employerSchema)
 module.exports = Employer
