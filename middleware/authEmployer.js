effcb31ea85d828c4c346811aa12318a4fa200b5
unknown <promesse@octan.group>
2020-10-02 11:51:06 +0200

    Create employer's middleware file

---
+++ middleware/authEmployer.js
@@ -0,0 +1,20 @@
+const jw = require('jsonwebtoken')
+const Employer = require('../models/Employer')
+
+const authEmployer = async (req, res, next) => {
+    try {
+        const token = req.heaeder('Authorization').replace('Bearer ', '')
+        const decoded = jwt.verify(token, process.env.JWT_SECRET)
+        const employer = await Employer.findOne({ _id: decoded._id, 'tokens.token': token })
+        if (!employer) {
+            throw new Error()
+        }
+        req.token = token
+        req.employer = employer
+        next
+    } catch (error) {
+        res.status(401).send({ error: 'Please authenticate. ' })
+    }
+}
+
+module.exports = authEmployer
